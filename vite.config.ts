56d409cec671972cf3a03c7d9a09079ee5c25181
burakxrd <brk84055@gmail.com>
2026-03-29 22:13:26 +0300

    sitemap update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import Sitemap from 'vite-plugin-sitemap';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [
@@ -19,9 +19,11 @@ export default defineConfig(({mode}) => {
           '/tools/ip-intelligence',
           '/tools/domain-analyzer',
           '/tools/hash-generator',
-          '/tools/base64',
-          '/ctf',
+          '/tools/base64-codec',
+          '/tools/ctf-workspace',
           '/blog',
+          '/privacy-policy',
+          '/terms-of-service',
         ],
         exclude: ['/404'],
       }),
